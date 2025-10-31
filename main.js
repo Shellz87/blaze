f424e5ae7115b7f3589dad6e451370e6b1d9749f
Shellz87 <59041814+Shellz87@users.noreply.github.com>
2025-10-31 12:25:03 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+// js/main.js
+const menuToggle = document.querySelector('.menu-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+menuToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
